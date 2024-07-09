988f6cd6fd4b2020e7d343dbb12e5eb1f716eccf
GVerdans <149999884+GVerdans@users.noreply.github.com>
2024-07-08 22:17:03 -0300

    beggin os JS

--- sources/script/script.js
+++ sources/script/script.js
@@ -1,1 +1,26 @@
 'use strict'
+
+let inputTxtEntrada = document.querySelector('input.inputTxtEntrada');
+
+let ulLista = document.querySelector('ul.ulLista');
+
+function insertOnList() {
+
+    const newLi = document.createElement('li');
+    newLi.classList.add('listaLi');
+
+    // const btnLi = document.createElement('input');
+    // btnLi.type = "button";
+    // btnLi.value ="✔";
+    // btnLi.title = "Tarefa Concluída !";
+    // btnLi.onclick="doneList()";
+    // btnLi.classList.add('btnLi');
+
+    ulLista.appendChild(newLi);
+    newLi.innerHTML = `${inputTxtEntrada.value} <input type="button" value="✔" class="btnLi" title="Tarefa Concluída !" onclick="doneList()">`
+    
+    
+
+
+
+}

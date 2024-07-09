54669eb722f22fa048070ade78ec9b54221d255d
GVerdans <149999884+GVerdans@users.noreply.github.com>
2024-07-09 19:42:08 -0300

    pause to study function

--- sources/script/script.js
+++ sources/script/script.js
@@ -4,23 +4,23 @@ let inputTxtEntrada = document.querySelector('input.inputTxtEntrada');
 
 let ulLista = document.querySelector('ul.ulLista');
 
+
 function insertOnList() {
 
-    const newLi = document.createElement('li');
-    newLi.classList.add('listaLi');
+        const newLi = document.createElement('li');
+        newLi.classList.add('listaLi');
 
-    // const btnLi = document.createElement('input');
-    // btnLi.type = "button";
-    // btnLi.value ="✔";
-    // btnLi.title = "Tarefa Concluída !";
-    // btnLi.onclick="doneList()";
-    // btnLi.classList.add('btnLi');
+        // const btnLi = document.createElement('input');
+        // btnLi.type = "button";
+        // btnLi.value ="✔";
+        // btnLi.title = "Tarefa Concluída !";
+        // btnLi.onclick="doneList()";
+        // btnLi.classList.add('btnLi');
 
-    ulLista.appendChild(newLi);
-    newLi.innerHTML = `${inputTxtEntrada.value} <input type="button" value="✔" class="btnLi" title="Tarefa Concluída !" onclick="doneList()">`
-    
-    
+        ulLista.appendChild(newLi);
+        newLi.innerHTML = `${inputTxtEntrada.value} <input type="button" value="✔" class="btnLi" title="Tarefa Concluída !" onclick="doneList()">`
 
 
-
-}
+        inputTxtEntrada.value = ''
+        inputTxtEntrada.focus()
+    }

4b7d8798669de5e8125470f9779ba74e06861a04
GVerdans <149999884+GVerdans@users.noreply.github.com>
2024-07-09 21:45:00 -0300

    falta descobrir como excluir cada tag li

--- sources/script/script.js
+++ sources/script/script.js
@@ -4,23 +4,40 @@ let inputTxtEntrada = document.querySelector('input.inputTxtEntrada');
 
 let ulLista = document.querySelector('ul.ulLista');
 
+function isInputVazio(texto) {
+    if (String(texto).length < 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function insertOnList() {
 
+    if (isInputVazio(inputTxtEntrada.value)) {
+        window.alert('Insira um texto arrombado !')
+
+    } else {
+
         const newLi = document.createElement('li');
         newLi.classList.add('listaLi');
-
-        // const btnLi = document.createElement('input');
-        // btnLi.type = "button";
-        // btnLi.value ="✔";
-        // btnLi.title = "Tarefa Concluída !";
-        // btnLi.onclick="doneList()";
-        // btnLi.classList.add('btnLi');
-
         ulLista.appendChild(newLi);
+
         newLi.innerHTML = `${inputTxtEntrada.value} <input type="button" value="✔" class="btnLi" title="Tarefa Concluída !" onclick="doneList()">`
 
 
         inputTxtEntrada.value = ''
         inputTxtEntrada.focus()
     }
+}
+
+function doneList() {
+
+    const paiUl = document.querySelector('ul.ulLista');
+    const newLi = document.querySelector('li.listaLi');
+
+
+    paiUl.removeChild(newLi);
+
+    ulLista.innerHTML
+}
